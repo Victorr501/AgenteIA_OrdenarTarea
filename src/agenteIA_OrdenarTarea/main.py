a5bb14bcf3c15dd88f786c737f36bc427d2cdcd3
Víctor Rubín Rubio <VrubinR501@outlook.com>
2026-02-03 23:15:30 +0100

    3/1/2026

--- src/agenteIA_OrdenarTarea/main.py
+++ src/agenteIA_OrdenarTarea/main.py
@@ -1,15 +1,35 @@
 from __future__ import annotations
 
-import argparse
-import json
-from dataclasses import dataclass, asdict
-from datetime import datetime, timezone
-from pathlib import Path
-from typing import Literal
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-
-
+# 1.Cargar varialbes de entorno (.env)
+load_dotenv()
 
 def run() -> None:
-    print("Hola mundo")
+    print("--- Iniciando Prueba de Conexión con Gemini ---")
+    
+    api_key = os.getenv("GOOGLE_API_KEY")
+    if not api_key:
+        print("ERROR: No se encontró la GOOGLE_API_KEY")
+        return
+    
+    
+    try:
+        # Intentar una llamada directa
+        llm = ChatGoogleGenerativeAI(
+            model = "gemini-2.0-flash",
+            google_api_key = api_key
+        )
+        response = llm.invoke("Hola, ¿estás activo?")
+        print(f"\n Respuesta directa: {response.text}")
+        print("\n --- ¡CONEXIÓN EXITOSA! ---")
+        
+    except Exception as e:
+        print(f"\n Error de diagnóstico: {e}")
+
+
+if __name__ == "__main__":
+    run()
     
